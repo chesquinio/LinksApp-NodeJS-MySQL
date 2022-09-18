1e385347849a63efc7ca1b242e8acf4032c6c952
francis <willenerfrancis0@gmail.com>
2022-09-18 00:19:08 -0300

    Coneccion con DB MySQL

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express();
 // settings
 
 app.set('port', process.env.PORT || 4000);
-app.use('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', engine({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
@@ -23,14 +23,24 @@ app.engine('.hbs', engine({
 // Middlewares
 
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 
 // Global Variables
+
+app.use((req, res, next) => {
+    
+    next();
+});
 
 // Routes
 
 app.use(require('./routes'));
+app.use(require('./routes/authentication'));
+app.use('/links', require('./routes/links'));
 
 // Public
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Starting the Server
 app.listen(app.get('port'), ()=> {
