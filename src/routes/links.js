3329a0ba0871c862f933b0d84572f6fb03a2cbf6
francis <willenerfrancis0@gmail.com>
2022-09-18 15:42:11 -0300

    Mostrar y Eliminar Links

--- src/routes/links.js
+++ src/routes/links.js
@@ -14,8 +14,19 @@ router.post('/add', async(req, res)=> {
         url,
         description
     };
-    pool.query('INSERT INTO links set ?', [newLink]);
-    res.send('recived');
+    await pool.query('INSERT INTO links set ?', [newLink]);
+    res.redirect('/links');
+})
+
+router.get('/', async (req, res)=> {
+    const links = await pool.query('SELECT * FROM links');
+    res.render('links/list', { links });
+});
+
+router.get('/delete/:id', async (req, res)=> {
+    const { id } = req.params;
+    await pool.query('DELETE FROM links WHERE ID = ?', [id]);
+    res.redirect('/links');
 })
 
 module.exports = router;
