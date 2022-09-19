66530b72b96b428a2bcba66bb26a521d874dbf79
francis <willenerfrancis0@gmail.com>
2022-09-19 11:38:14 -0300

    SignUp terminado

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ const flash = require('connect-flash');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session');
 const {database} = require('./keys')
+const passport = require('passport')
 
 // initializations
 
 const app = express();
+require('./lib/passport');
 
 // settings
 
@@ -36,6 +38,8 @@ app.use(flash());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 // Global Variables
